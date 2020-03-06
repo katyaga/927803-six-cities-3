b7cd8fa01423f7a782a87b8cbbbfb423dfaaaea7
user <aevakatya@gmail.com>
2020-03-06 09:28:27 +0700

    Исправлен контент компонента

--- src/components/without-offers/without-offers.jsx
+++ src/components/without-offers/without-offers.jsx
@@ -6,7 +6,7 @@ const WithoutOffers = () => {
       <section className="cities__no-places">
         <div className="cities__status-wrapper tabs__content">
           <b className="cities__status">No places to stay available</b>
-          <p className="cities__status-description">We could not find any property availbale at the
+          <p className="cities__status-description">We could not find any property available at the
             moment in Dusseldorf</p>
         </div>
       </section>
